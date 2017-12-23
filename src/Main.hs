ff57b04a87a2ed8dcefcba3311afe2c88cbca7ca
Christophe <ch.alexandre@bluewin.ch>
2017-12-23 18:29:55 +0100

    dev

--- src/Main.hs
+++ src/Main.hs
@@ -3,14 +3,13 @@
 module Main where
 
 import Prelude (IO, Ord, Ordering, Bool, fmap, compare, ($), filter, print, mapM)
-import Codec.Archive.Zip
-import Path
+import Codec.Archive.Zip (EntrySelector, ZipArchive, withArchive, getEntry, getEntries, getEntryName)
+import Path (Path, Abs, File)
 import Data.Maybe (isJust)
 import Path.IO (resolveFile')
 import Data.List (sortBy)
 import Data.Text (Text, unpack)
-import Text.Regex
-import Data.Ord (comparing)
+import Text.Regex (Regex, matchRegex, mkRegex)
 import Data.ByteString.Char8 (concat)
 import Data.Map (keys)
 import Data.ByteString (ByteString)
@@ -27,17 +26,17 @@ extractEntries ticksArchivePath = withArchive ticksArchivePath loadEntries
 customSort :: Ord a => a -> a -> Ordering
 customSort elem1 elem2 = compare elem1 elem2
 
-isCSVFile :: EntrySelector -> Bool
-isCSVFile entry = isJust $ matchRegex csvPattern (unpack entryName)
+isTickFile :: EntrySelector -> Bool
+isTickFile entry = isJust $ matchRegex tickFilePattern (unpack entryName)
     where
-        csvPattern = mkRegex "/20.*csv$" :: Regex
+        tickFilePattern = mkRegex "/20.*csv$" :: Regex
         entryName = getEntryName entry :: Text
 
 main :: IO ()
 main = do
     ticksArchivePath <- resolveFile' "data/data-small.zip" :: IO (Path Abs File)
     entries <- extractEntries ticksArchivePath :: IO [EntrySelector]
-    let csvEntries = sortBy customSort $ filter isCSVFile entries :: [EntrySelector]
+    let csvEntries = sortBy customSort $ filter isTickFile entries :: [EntrySelector]
     print csvEntries
     let ticks = loadTicks ticksArchivePath :: EntrySelector -> IO ByteString
     contents <- mapM ticks csvEntries :: IO [ByteString]
