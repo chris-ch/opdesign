285896e032ecd07691b38a7a92deb8ed1fbf3da6
Christophe <ch.alexandre@bluewin.ch>
2017-12-27 15:11:36 +0100

    dev

--- src/Main.hs
+++ src/Main.hs
@@ -2,28 +2,25 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Prelude (IO, Ord, Ordering, Bool, fmap, compare, ($), (==), (++), filter, print, mapM, putStrLn, show, return)
+import Prelude (IO, Ord, Ordering, Bool, String, fmap, compare, (.), ($), (==), (++), filter, print, mapM, putStrLn, show, return)
 import Codec.Archive.Zip (EntrySelector, ZipArchive, withArchive, sourceEntry, getEntries, getEntryName)
 import Path (Path, Abs, File)
 import Data.Maybe (isJust)
 import Path.IO (resolveFile')
-import Data.List (sortBy)
+import Data.List (sortBy, dropWhileEnd)
 import Data.Text (Text, pack, unpack, splitOn)
-import Data.Text.Encoding (decodeUtf8)
 import Text.Regex (Regex, matchRegex, mkRegex)
-import Data.ByteString.Char8 (concat)
 import Data.Map (keys)
-import Data.ByteString (ByteString)
 import Control.Monad.IO.Class (liftIO)
-import Data.Conduit
+import Data.Conduit ((=$=))
 import qualified Data.Conduit.List as CL
 import qualified Data.Conduit.Text as CT
 
-loadTicks :: Path Abs File -> EntrySelector -> IO ()
-loadTicks ticksArchivePath entry = withArchive ticksArchivePath $ do
+processTicks :: Path Abs File -> (Text -> IO()) -> EntrySelector -> IO ()
+processTicks ticksArchivePath processLine entry = withArchive ticksArchivePath $ do
     sourceEntry entry $ CT.decode CT.utf8
-            =$ CT.lines
-            =$ CL.mapM_ (\t -> liftIO $ putStrLn $ "Got a line: " ++ show t)
+            =$= CT.lines
+            =$= CL.mapM_ (\line -> liftIO $ processLine line)
     
 extractEntries :: Path Abs File -> IO [EntrySelector]
 extractEntries ticksArchivePath = withArchive ticksArchivePath loadEntries
@@ -34,23 +31,21 @@ extractEntries ticksArchivePath = withArchive ticksArchivePath loadEntries
 customSort :: Ord a => a -> a -> Ordering
 customSort elem1 elem2 = compare elem1 elem2
 
-isTickFile :: EntrySelector -> Bool
-isTickFile entry = isJust $ matchRegex tickFilePattern (unpack entryName)
+isTickFile :: String -> EntrySelector -> Bool
+isTickFile tickFilePattern entry = isJust $ matchRegex (mkRegex tickFilePattern) (unpack entryName)
     where
-        tickFilePattern = mkRegex "20.*csv$" :: Regex
         entryName = getEntryName entry :: Text
+
+dos2unix :: Text -> Text
+dos2unix = pack . dropWhileEnd (== '\r') . unpack
 
 main :: IO ()
 main = do
-    --ticksArchivePath <- resolveFile' "data/data-small.zip" :: IO (Path Abs File)
-    ticksArchivePath <- resolveFile' "data/GX1%20Index.zip" :: IO (Path Abs File)
+    ticksArchivePath <- resolveFile' "data/data-small.zip" :: IO (Path Abs File)
+    --ticksArchivePath <- resolveFile' "data/GX1%20Index.zip" :: IO (Path Abs File)
     entries <- extractEntries ticksArchivePath :: IO [EntrySelector]
-    let csvEntries = sortBy customSort $ filter isTickFile entries :: [EntrySelector]
+    let csvEntries = sortBy customSort $ filter (isTickFile "/20.*csv$") entries :: [EntrySelector]
     print csvEntries
-    let ticks = loadTicks ticksArchivePath :: EntrySelector -> IO ()
+    let ticks = processTicks ticksArchivePath (print . dos2unix)
     contents <- mapM ticks csvEntries :: IO [()]
     return ()
-    --let ticks = decodeUtf8 $ concat contents :: Text
-    --let lineSep = pack "\r\n" :: Text
-    --print $ splitOn lineSep ticks
-    
