b0b9a79cfa27bf33e023de97328f4b9f8e634184
Christophe <ch.alexandre@bluewin.ch>
2019-02-13 07:41:29 +0100

    dev mergeSort

--- src/Data/Conduit/Merge.hs
+++ src/Data/Conduit/Merge.hs
@@ -1,6 +1,6 @@
 module Data.Conduit.Merge where
 
-import Prelude (Monad, Bool, Maybe)
+import Prelude (Monad, Bool, Maybe(..))
 import Prelude (otherwise)
 import Control.Monad.State (lift)
 import Prelude (($))
@@ -13,17 +13,18 @@ mergeC2 :: (Monad m) => (a -> a -> Bool) -> ConduitT () a m () -> ConduitT () a 
 mergeC2 comparator (CI.ConduitT s1) (CI.ConduitT s2) = CI.ConduitT $  processMergeC2 comparator s1 s2
 
 -- newtype SealedConduitT i o m r = SealedConduitT (Pipe i i o () m r) 
--- newtype ConduitT i o m r = ConduitT { unConduitT :: forall b. (r -> Pipe i i o () m b) -> Pipe i i o () m b
+-- newtype ConduitT i o m r = ConduitT { runConduitT :: forall b. (r -> Pipe i i o () m b) -> Pipe i i o () m b
 processMergeC2 :: Monad m => (a -> a -> Bool)
                         -> ((() -> CI.Pipe () () a () m ()) -> CI.Pipe () () a () m ()) -- s1    ConduitT () a m ()
                         -> ((() -> CI.Pipe () () a () m ()) -> CI.Pipe () () a () m ()) -- s2    ConduitT () a m ()
                         -> ((() -> CI.Pipe () () a () m b ) -> CI.Pipe () () a () m b ) -- rest  ConduitT () a m ()
-processMergeC2 comparator s1 s2 rest = let
-        go right@(CI.HaveOutput s1' v1) left@(CI.HaveOutput s2' v2)
-            | comparator v1 v2 = CI.HaveOutput (go s1' left) v1
-            | otherwise = CI.HaveOutput (go right s2') v2
-        go right@CI.Done{} (CI.HaveOutput s v) = CI.HaveOutput (go right s) v
-        go (CI.HaveOutput s v) left@CI.Done{}  = CI.HaveOutput (go s left)  v
+processMergeC2 comparator s1 s2 rest = go (s1 CI.Done) (s2 CI.Done)
+    where
+        go s1''@(CI.HaveOutput s1' v1) s2''@(CI.HaveOutput s2' v2)  -- s1''@ and s2''@ simply name the pattern expressions
+            | comparator v1 v2 = CI.HaveOutput (go s1' s2'') v1
+            | otherwise = CI.HaveOutput (go s1'' s2') v2
+        go s1'@CI.Done{} (CI.HaveOutput s v) = CI.HaveOutput (go s1' s) v
+        go (CI.HaveOutput s v) s1'@CI.Done{}  = CI.HaveOutput (go s s1')  v
         go CI.Done{} CI.Done{} = rest ()
         go (CI.PipeM p) left = do
             next <- lift p
@@ -35,19 +36,22 @@ processMergeC2 comparator s1 s2 rest = let
         go right (CI.NeedInput _ next) = go right (next ())
         go (CI.Leftover next ()) left = go next left
         go right (CI.Leftover next ()) = go right next
-    in go (s1 CI.Done) (s2 CI.Done)
 
 -- | Takes two monotonic sources and merges them.
 --
--- zipC2 :: (Monad m) => (a -> a -> Bool) -> ConduitT () a m () -> ConduitT () a m () -> ConduitT () (Maybe a, Maybe a) m ()
+-- zipC2 :: (Monad m) => (Maybe a -> Maybe a -> Bool) -> ConduitT () a m () -> ConduitT () a m () -> ConduitT () (Maybe a, Maybe a) m ()
 -- zipC2 comparator (CI.ConduitT s1) (CI.ConduitT s2) = CI.ConduitT $ processZipC2 comparator s1 s2
-
--- processZipC2 comparator s1 s2 rest = let
---         go right@(CI.HaveOutput s1' v1) left@(CI.HaveOutput s2' v2)
---             | comparator v1 v2 = CI.HaveOutput (go s1' left) v1
---             | otherwise = CI.HaveOutput (go right s2') v2
---         go right@CI.Done{} (CI.HaveOutput s v) = CI.HaveOutput (go right s) v
---         go (CI.HaveOutput s v) left@CI.Done{}  = CI.HaveOutput (go s left)  v
+-- processZipC2 :: Monad m => (Maybe a -> Maybe a -> Bool)
+--                         -> ((() -> CI.Pipe () () (Maybe a) () m ()) -> CI.Pipe () () (Maybe a) () m ()) -- s1    ConduitT () a m ()
+--                         -> ((() -> CI.Pipe () () (Maybe a) () m ()) -> CI.Pipe () () (Maybe a) () m ()) -- s2    ConduitT () a m ()
+--                         -> ((() -> CI.Pipe () () (Maybe a, Maybe a) () m b ) -> CI.Pipe () () (Maybe a, Maybe a) () m b ) -- rest  ConduitT () a m ()
+-- processZipC2 comparator s1 s2 rest = go (s1 CI.Done) (s2 CI.Done)
+--     where
+--         go s1''@(CI.HaveOutput s1' v1) s2''@(CI.HaveOutput s2' v2)  -- s1''@ and s2''@ simply name the pattern expressions
+--             | comparator v1 v2 = CI.HaveOutput (go s1' s2'') (Nothing, Nothing)
+--             | otherwise = CI.HaveOutput (go s1'' s2') (Nothing, Nothing)
+--         go s1'@CI.Done{} (CI.HaveOutput s v) = CI.HaveOutput (go s1' s) v
+--         go (CI.HaveOutput s v) s1'@CI.Done{}  = CI.HaveOutput (go s s1') v
 --         go CI.Done{} CI.Done{} = rest ()
 --         go (CI.PipeM p) left = do
 --             next <- lift p
@@ -59,4 +63,3 @@ processMergeC2 comparator s1 s2 rest = let
 --         go right (CI.NeedInput _ next) = go right (next ())
 --         go (CI.Leftover next ()) left = go next left
 --         go right (CI.Leftover next ()) = go right next
---     in go (s1 CI.Done) (s2 CI.Done)
--- test/Data/Conduit/MergeSpec.hs
+++ test/Data/Conduit/MergeSpec.hs
@@ -1,40 +1,65 @@
 module Data.Conduit.MergeSpec where
 
-    import SpecHelper
-    
-    import Prelude (Int, Integer)
-    import Prelude (zipWith, drop, maybe, return)
-    import Prelude (($), (<=))
-    import Conduit ()
-    import Conduit (yieldMany, runConduitPure, takeC)
-    import Conduit (sinkList)
-    import Conduit ((.|))
+import SpecHelper
 
-    import Data.Conduit.Merge (mergeC2) --, zipC2)
-    
-    spec :: Spec
-    spec = describe "Testing Conduit merging operators" $ do
-    
-        context "yielding array of 10 first integers" $
-            it "should yield many" $ 
-                runConduitPure (yieldMany [1..10 :: Integer] .| sinkList)
-            `shouldBe` [1..10]
-    
-        context "merging sources (assumes both are monotonic)" $
-            let
-                naturals1 = yieldMany [10, 20, 30, 40, 50, 60, 70, -10, -20, -10 :: Int]
-                naturals2 = yieldMany [1, 2, 3, 4, 5 :: Int]
-            in
-            it "should zip 2 sources" $
-                runConduitPure ( mergeC2 (<=) naturals1 naturals2 .| takeC 100 .| sinkList )
-                `shouldBe` [1, 2, 3, 4, 5, 10, 20, 30, 40, 50, 60, 70, -10, -20, -10]
-    
-        -- context "zipping sources as pairs" $
-        --     let
-        --         naturals1 = yieldMany [2, 4, 6, 8, 16 :: Int]
-        --         naturals2 = yieldMany [1, 5, 6 :: Int]
-        --     in
-        --     it "should zip 2 sources" $
-        --         runConduitPure ( zipC2 (<=) naturals1 naturals2 .| takeC 100 .| sinkList )
-        --         `shouldBe` [(Nothing, Just 1), (Just 2, Just 1), (Just 4, Just 1), (Just 4, Just 5), (Just 6, Just 5), (Just 6, Just 6), (Just 8, Just 6), (Just 16, Just 6)]
-    
+import Prelude (Int, Integer, Maybe(..), Bool, Monad, Show, Eq)
+import Prelude (zipWith, drop, maybe, return)
+import Prelude (($), (<=), (>=))
+import Conduit (ConduitT)
+import Conduit (yieldMany, runConduitPure, takeC, mapC)
+import Conduit (sinkList)
+import Conduit ((.|))
+import Data.Conduit.Internal (zipSources)
+--import Data.Conduit.Algorithms (mergeC2)
+
+import Data.Conduit.Merge (mergeC2) --, zipC2)
+
+spec :: Spec
+spec = describe "Testing Conduit merging operators" $ do
+
+    context "yielding array of 10 first integers" $
+        it "should yield many" $ 
+            runConduitPure (yieldMany [1..10 :: Integer] .| sinkList)
+        `shouldBe` [1..10]
+
+    context "merging sources (assumes both are monotonic)" $
+        let
+            naturals1 = yieldMany [10, 20, 30, 40, 50, 60, 70, -10, -20, -10 :: Int]
+            naturals2 = yieldMany [1, 2, 3, 4, 5 :: Int]
+        in
+        it "should zip 2 sources" $
+            runConduitPure ( mergeC2 (<=) naturals1 naturals2 .| takeC 100 .| sinkList )
+            `shouldBe` [1, 2, 3, 4, 5, 10, 20, 30, 40, 50, 60, 70, -10, -20, -10]
+
+    context "merging monotonic sources with sorting" $
+        let
+            series1 = yieldMany [2, 4, 6, 8, 16 :: Int]
+            series2 = yieldMany [1, 5, 6 :: Int]
+        in
+        it "should zip 2 sources" $
+            runConduitPure ( (mergeTag (<=) series1 series2) .| sinkList )
+            `shouldBe` [TaggedItem RightItem 1, TaggedItem LeftItem 2, TaggedItem LeftItem 4, 
+                TaggedItem RightItem 5, TaggedItem LeftItem 6, TaggedItem RightItem 6, TaggedItem LeftItem 8, TaggedItem LeftItem 16]
+
+    context "zipping sources as pairs" $
+        let
+            series1 = yieldMany [2, 4, 6, 8, 16 :: Int]
+            series2 = yieldMany [1, 5, 6 :: Int]
+        in
+        it "should zip 2 sources" $
+            runConduitPure ( (mergeTag (<=) series1 series2) .| pairTag .| sinkList )
+            `shouldBe` [(Nothing, Just 1), (Just 2, Just 1), (Just 4, Just 1), (Just 4, Just 5), (Just 6, Just 5), (Just 6, Just 6), (Just 8, Just 6), (Just 16, Just 6)]
+
+data MergeTag = LeftItem | RightItem deriving (Show, Eq)
+data TaggedItem a = TaggedItem MergeTag a deriving (Show, Eq)
+mergeTag :: (Monad m) => (a -> a -> Bool) -> ConduitT () a m () -> ConduitT () a m () -> ConduitT () (TaggedItem a) m ()
+mergeTag func series1 series2 = mergeC2 (tagSort func) taggedSeries1 taggedSeries2
+                where
+                    taggedSeries1 = series1 .| mapC (\item -> TaggedItem LeftItem item)
+                    taggedSeries2 = series2 .| mapC (\item -> TaggedItem RightItem item)
+                    
+tagSort :: (a -> a -> Bool) -> TaggedItem a -> TaggedItem a -> Bool
+tagSort func (TaggedItem tag1 item1) (TaggedItem tag2 item2) = func item1 item2
+
+pairTag :: (Monad m) => ConduitT  (TaggedItem a) (Maybe a, Maybe a) m ()
+pairTag = yieldMany [(Nothing, Nothing)]
