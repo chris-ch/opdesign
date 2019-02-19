bb876b463ef5724df57ee7ee8cdacd9516165422
Christophe <ch.alexandre@bluewin.ch>
2019-02-19 18:05:48 +0100

    working

--- src/Data/Conduit/Log.hs
+++ src/Data/Conduit/Log.hs
@@ -2,7 +2,7 @@ module Data.Conduit.Log where
 
 import Prelude (Maybe(..), IO, FilePath, Show)
 import Prelude (show, appendFile, return)
-import Prelude (($))
+import Prelude (($), (++))
 import Conduit (ConduitT, MonadIO)
 import Conduit (await, liftIO, yield)
 
@@ -12,7 +12,7 @@ logC filename = do
     case maybeItem of
         Nothing -> return ()
         Just item -> do
-            liftIO (appendFile filename (show item))
+            liftIO (appendFile filename ((show item) ++ "\n"))
             yield item
             logC filename
             
