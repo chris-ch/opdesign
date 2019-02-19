0936a114921c299604463f88c2200f07b5f13690
Christophe <ch.alexandre@bluewin.ch>
2019-02-19 18:04:03 +0100

    working

---
+++ src/Data/Conduit/Log.hs
@@ -0,0 +1,18 @@
+module Data.Conduit.Log where
+
+import Prelude (Maybe(..), IO, FilePath, Show)
+import Prelude (show, appendFile, return)
+import Prelude (($))
+import Conduit (ConduitT, MonadIO)
+import Conduit (await, liftIO, yield)
+
+logC :: (MonadIO m, Show a) => FilePath -> ConduitT a a m ()
+logC filename = do
+    maybeItem <-  await
+    case maybeItem of
+        Nothing -> return ()
+        Just item -> do
+            liftIO (appendFile filename (show item))
+            yield item
+            logC filename
+            
