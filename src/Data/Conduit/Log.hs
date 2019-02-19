dbbadb6b469f9ffb985d34f40cee4143419342aa
Christophe <ch.alexandre@bluewin.ch>
2019-02-19 19:13:06 +0100

    dev

--- src/Data/Conduit/Log.hs
+++ src/Data/Conduit/Log.hs
@@ -1,10 +1,10 @@
 module Data.Conduit.Log where
 
-import Prelude (Maybe(..), FilePath, Show)
+import Prelude (Maybe(..), FilePath, Show, IO)
 import Prelude (show, return)
-import Prelude (($))
+import Prelude (($), (++))
 import System.IO (hPutStrLn, hClose, openFile, IOMode(..))
-
+import Data.Time.Clock (UTCTime, getCurrentTime)
 import Conduit (ConduitT, MonadIO)
 import Conduit (await, liftIO, yield)
 
@@ -20,7 +20,8 @@ logC filename = do
                     liftIO $ hClose h
                     return ()
                 Just item -> do
-                    liftIO $ hPutStrLn h (show item)
+                    timestamp <- liftIO (getCurrentTime :: IO UTCTime)
+                    liftIO $ hPutStrLn h (show timestamp ++ " - " ++ show item)
                     yield item
                     go h
             
